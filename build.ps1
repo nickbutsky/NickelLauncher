b40a33c697eab124a3a60943d6cb0303e95a926b
Nickel59 <91761951+Nickel59@users.noreply.github.com>
2023-09-05 19:51:47 +0300

    add a build system (#3)

---
+++ build.ps1
@@ -0,0 +1,17 @@
+python.exe -m pip install --upgrade nuitka
+
+$config = ConvertFrom-Json -InputObject (Get-Content -Path "nuitka_config.json" -Raw)
+
+$scriptPath = $config."script-path"
+
+$arguments = ($config.arguments.PSObject.Properties | ForEach-Object {"--$($_.Name)=$($_.Value)"}) -join ' '
+
+$flags = $config.flags -join ' --'
+$flags = "--$flags"
+
+Invoke-Expression "nuitka $arguments $flags $scriptPath"
+
+$outputPath = "$($config."arguments"."output-dir")\$([System.IO.Path]::GetFileNameWithoutExtension($scriptPath)).dist"
+$scriptParentDirectoryName = (Split-Path $scriptPath -Parent) | Split-Path -Leaf
+
+Rename-Item -Path $outputPath -NewName $scriptParentDirectoryName
