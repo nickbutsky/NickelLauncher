1b908826aaf770795e7ce23cb6b39134f286723a
nbuts <nbutskyi@gmail.com>
2023-11-25 16:22:07 +0200

    store launcher data in %APPDATA%

--- NickelLauncher/env.py
+++ NickelLauncher/env.py
@@ -1,17 +1,11 @@
 from typing import Sequence
-import sys
 import os
 import shutil
 import subprocess
 import logging
 
 
-IS_IN_DEBUG_MODE = not ('__compiled__' in globals())
-
-if IS_IN_DEBUG_MODE:
-    LAUNCHER_DATA_DIR_PATH = os.path.join(os.getenv('APPDATA'), 'NickelLauncher')
-else:
-    LAUNCHER_DATA_DIR_PATH = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), 'data')
+LAUNCHER_DATA_DIR_PATH = os.path.join(os.getenv('APPDATA'), 'NickelLauncher')
 
 VERSIONS_DIR_PATH = os.path.join(LAUNCHER_DATA_DIR_PATH, 'versions')
 INSTANCES_DIR_PATH = os.path.join(LAUNCHER_DATA_DIR_PATH, 'instances')
