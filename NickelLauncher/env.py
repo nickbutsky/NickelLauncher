15f4e945d5607b52edbc8790ae5a5e5958cc2ab8
nbuts <nbutskyi@gmail.com>
2023-11-25 16:24:13 +0200

    store test launcher data in a different location

--- NickelLauncher/env.py
+++ NickelLauncher/env.py
@@ -5,7 +5,12 @@ import subprocess
 import logging
 
 
-LAUNCHER_DATA_DIR_PATH = os.path.join(os.getenv('APPDATA'), 'NickelLauncher')
+IS_IN_DEBUG_MODE = not ('__compiled__' in globals())
+
+if IS_IN_DEBUG_MODE:
+    LAUNCHER_DATA_DIR_PATH = os.path.join(os.getenv('APPDATA'), 'NickelLauncherTest')
+else:
+    LAUNCHER_DATA_DIR_PATH = os.path.join(os.getenv('APPDATA'), 'NickelLauncher')
 
 VERSIONS_DIR_PATH = os.path.join(LAUNCHER_DATA_DIR_PATH, 'versions')
 INSTANCES_DIR_PATH = os.path.join(LAUNCHER_DATA_DIR_PATH, 'instances')
