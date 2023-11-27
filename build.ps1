9e7572b72f73169662b59c66b66ea03d826c8532
nbuts <nbutskyi@gmail.com>
2023-11-27 12:21:48 +0200

    rename a variable

--- build.ps1
+++ build.ps1
@@ -1,17 +1,17 @@
 python.exe -m pip install Nuitka==1.6.6
 
-$config = ConvertFrom-Json -InputObject (Get-Content -Path "nuitka_config.json" -Raw)
+$nuitkaConfig = ConvertFrom-Json -InputObject (Get-Content -Path "nuitka_config.json" -Raw)
 
-$scriptPath = $config."script-path"
+$scriptPath = $nuitkaConfig."script-path"
 
-$arguments = ($config.arguments.PSObject.Properties | ForEach-Object {"--$($_.Name)=$($_.Value)"}) -join ' '
+$arguments = ($nuitkaConfig.arguments.PSObject.Properties | ForEach-Object {"--$($_.Name)=$($_.Value)"}) -join ' '
 
-$flags = $config.flags -join ' --'
+$flags = $nuitkaConfig.flags -join ' --'
 $flags = "--$flags"
 
 Invoke-Expression "nuitka $arguments $flags $scriptPath"
 
-$outputPath = "$($config."arguments"."output-dir")\$([System.IO.Path]::GetFileNameWithoutExtension($scriptPath)).dist"
+$outputPath = "$($nuitkaConfig."arguments"."output-dir")\$([System.IO.Path]::GetFileNameWithoutExtension($scriptPath)).dist"
 $scriptParentDirectoryName = (Split-Path $scriptPath -Parent) | Split-Path -Leaf
 
 Rename-Item -Path $outputPath -NewName $scriptParentDirectoryName
