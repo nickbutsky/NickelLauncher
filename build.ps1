f11a15fc6f84c3aaa61d522317fc2b28dc0c7dd0
nbuts <nbutskyi@gmail.com>
2023-11-29 01:44:10 +0200

    delete dist before compiling again

--- build.ps1
+++ build.ps1
@@ -3,11 +3,10 @@ python.exe -m pip install Nuitka==1.6.6
 $nuitkaConfig = ConvertFrom-Json -InputObject (python -c "import json, json_ref_dict; print(json.dumps(json_ref_dict.materialize(json_ref_dict.RefDict('nuitka_config.json'))))")
 
 $scriptPath = $nuitkaConfig."script-path"
+$arguments = ($nuitkaConfig.arguments.PSObject.Properties | ForEach-Object {"--$($_.Name)=$($_.Value)"}) -join " "
+$flags = "--$($nuitkaConfig.flags -join " --")"
 
-$arguments = ($nuitkaConfig.arguments.PSObject.Properties | ForEach-Object {"--$($_.Name)=$($_.Value)"}) -join ' '
-
-$flags = $nuitkaConfig.flags -join ' --'
-$flags = "--$flags"
+if (Test-Path "dist") { Remove-Item "dist" -Recurse }
 
 Invoke-Expression "nuitka $arguments $flags $scriptPath"
 
