4e244b647e2868653a0eceda67a3ae57ed7ec287
nbuts <nbutskyi@gmail.com>
2023-11-29 00:00:48 +0200

    resolve references in nuitka_config.json before parsing it

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 python.exe -m pip install Nuitka==1.6.6
 
-$nuitkaConfig = ConvertFrom-Json -InputObject (Get-Content -Path "nuitka_config.json" -Raw)
+$nuitkaConfig = ConvertFrom-Json -InputObject (python -c "import json, json_ref_dict; print(json.dumps(json_ref_dict.materialize(json_ref_dict.RefDict('nuitka_config.json'))))")
 
 $scriptPath = $nuitkaConfig."script-path"
 
