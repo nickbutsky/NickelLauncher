5a6e0ac328b4b428cdce8003a6af7f8496868194
nbuts <nbutskyi@gmail.com>
2023-11-29 13:27:17 +0200

    simplify code

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+$appConfig = ConvertFrom-Json -InputObject (Get-Content -Path "app_config.json" -Raw)
 $nuitkaConfig = ConvertFrom-Json -InputObject (python -c "import json, json_ref_dict; print(json.dumps(json_ref_dict.materialize(json_ref_dict.RefDict('nuitka_config.json'))))")
 
 $scriptPath = $nuitkaConfig."script-path"
@@ -8,7 +9,4 @@ if (Test-Path "dist") { Remove-Item "dist" -Recurse }
 
 Invoke-Expression "nuitka $arguments $flags $scriptPath"
 
-$outputPath = "$($nuitkaConfig."arguments"."output-dir")\$([System.IO.Path]::GetFileNameWithoutExtension($scriptPath)).dist"
-$scriptParentDirectoryName = (Split-Path $scriptPath -Parent) | Split-Path -Leaf
-
-Rename-Item -Path $outputPath -NewName $scriptParentDirectoryName
+Rename-Item -Path "dist\$([System.IO.Path]::GetFileNameWithoutExtension($scriptPath)).dist" -NewName $appConfig."name"
