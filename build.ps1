bca5d95f45b54fd72e1fb025ce09a576489d0973
nbuts <nbutskyi@gmail.com>
2023-11-30 14:34:47 +0200

    download dll dependencies if needed

--- build.ps1
+++ build.ps1
@@ -14,5 +14,14 @@ Invoke-Expression "nuitka $arguments $flags $scriptPath"
 Rename-Item -Path "dist\$([System.IO.Path]::GetFileNameWithoutExtension($scriptPath)).dist" -NewName $appConfig."name"
 
 if ($PSBoundParameters.ContainsKey('isccPath')) {
+    if (-not (Test-Path "Unins.dll")) {
+        $tempFolder = [System.IO.Path]::GetTempPath() + [System.IO.Path]::GetRandomFileName()
+        New-Item -ItemType Directory -Path $tempFolder
+        Invoke-WebRequest -Uri "https://github.com/Bill-Stewart/UninsIS/releases/download/v1.0.1/UninsIS-1.0.1.zip" -OutFile "$tempFolder\UninsIS-1.0.1.zip"
+        Expand-Archive -Path "$tempFolder\UninsIS-1.0.1.zip" -DestinationPath $tempFolder
+        Move-Item -Path "$tempFolder\UninsIS.dll" -Destination (Get-Location).Path
+        Remove-Item -Path $tempFolder -Recurse
+    }
+
     Invoke-Expression "&'$isccPath' /DAppName=$( $appConfig.'name' ) /DAppPublisher=$( $appConfig.'publisher' ) /DAppVersion=$( $appConfig.'version' ) /DAppURL=$( $appConfig.'url' ) installer_config.iss"
 }
