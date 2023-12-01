0442875ae0a867065c95c4b7297e499e362a6f45
Nickel59 <91761951+Nickel59@users.noreply.github.com>
2023-12-01 12:56:15 +0200

    Hotfix (#7)

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ $appConfig = ConvertFrom-Json -InputObject (Get-Content -Path "app_config.json" 
 $nuitkaConfig = ConvertFrom-Json -InputObject (python -c "import json, json_ref_dict; print(json.dumps(json_ref_dict.materialize(json_ref_dict.RefDict('nuitka_config.json'))))")
 
 $scriptPath = $nuitkaConfig."script-path"
-$arguments = ($nuitkaConfig.arguments.PSObject.Properties | ForEach-Object {"--$($_.Name)=$($_.Value)"}) -join " "
+$arguments = ($nuitkaConfig.arguments.PSObject.Properties | ForEach-Object {"--$($_.Name)=`"$($_.Value)`""}) -join " "
 $flags = "--$($nuitkaConfig.flags -join " --")"
 if (Test-Path "dist") { Remove-Item "dist" -Recurse }
 Invoke-Expression "nuitka $arguments $flags $scriptPath"
@@ -20,5 +20,5 @@ if ($PSBoundParameters.ContainsKey('isccPath')) {
         Remove-Item -Path $tempFolder -Recurse
     }
 
-    Invoke-Expression "&'$isccPath' /DAppName=$( $appConfig.'name' ) /DAppPublisher=$( $appConfig.'publisher' ) /DAppVersion=$( $appConfig.'version' ) /DAppURL=$( $appConfig.'url' ) installer_config.iss"
+    Invoke-Expression "&'$isccPath' /DAppName=`"$($appConfig.'name')`" /DAppPublisher=`"$($appConfig.'publisher')`" /DAppVersion=`"$($appConfig.'version')`" /DAppURL=`"$($appConfig.'url')`" installer_config.iss"
 }
