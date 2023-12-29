27fd083d609735a8f85b61fb184e7c1ac374d2c5
nbuts <nbutskyi@gmail.com>
2023-12-29 22:37:06 +0200

    rewrite versionmanager

---
+++ NickelLauncher/versionretrieve.py
@@ -0,0 +1,75 @@
+from copy import copy
+import os
+import json
+
+from schema import Schema
+import requests
+
+from env import VERSIONS_DIR_PATH
+from core.version import Version
+
+
+SUPPORTED_ARCHITECTURES = ['x64', 'x86']
+
+_VERSIONS_JSON_PATH = os.path.join(VERSIONS_DIR_PATH, 'versions.json')
+
+_versions: list[Version] = []
+
+
+def get_versions_locally() -> list[Version]:
+    global _versions
+    if _versions:
+        return copy(_versions)
+    versions = _parse_versions_json_contents(_load_versions_json())
+    _versions = versions
+    return copy(_versions)
+
+
+def get_versions_remotely() -> list[Version]:
+    global _versions
+    res = requests.get('https://raw.githubusercontent.com/dummydummy123456/BedrockDB/main/versions.json')
+    with open(_VERSIONS_JSON_PATH, 'w') as f:
+        f.write(res.text)
+    _versions = _parse_versions_json_contents(json.loads(res.text))
+    return copy(_versions)
+
+
+def _parse_versions_json_contents(contents: list[dict]) -> list[Version]:
+    return [
+        Version(
+            item['name'],
+            item['type'],
+            {
+                architecture: guids for architecture, guids in item['guids'].items()
+                if guids and architecture in SUPPORTED_ARCHITECTURES
+            },
+            {
+                architecture: os.path.join(VERSIONS_DIR_PATH, f"{item['name']}_{architecture}.Appx")
+                for architecture, guids in item['guids'].items() if guids and architecture in SUPPORTED_ARCHITECTURES
+            }
+        ) for item in contents
+    ]
+
+
+def _load_versions_json() -> list[dict]:
+    try:
+        with open(_VERSIONS_JSON_PATH) as f:
+            contents = json.load(f)
+    except (OSError, json.JSONDecodeError):
+        return []
+
+    if not Schema(
+        [
+            {
+                'name': str,
+                'type': str,
+                'guids': {
+                    'x64': [str],
+                    'x86': [str],
+                    'arm': [str]
+                }
+            }
+        ]
+    ).is_valid(contents):
+        return []
+    return contents
