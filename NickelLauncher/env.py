9d4542ae6638a99b1e367f2dc98e5c1b160c65d8
nbuts <nbutskyi@gmail.com>
2024-01-07 22:37:34 +0200

    migrate to pathlib

--- NickelLauncher/env.py
+++ NickelLauncher/env.py
@@ -1,13 +1,28 @@
 import os
+from pathlib import Path
+
+
+class Root(type(Path())):
+    @property
+    def versions(self) -> Path:
+        return Path(self) / 'versions'
+
+    @property
+    def instances(self) -> Path:
+        return Path(self) / 'instances'
+
+    @property
+    def temp(self) -> Path:
+        return Path(self) / 'temp'
+
+    @property
+    def logs(self) -> Path:
+        return Path(self) / 'logs'
+
 
 IS_IN_DEBUG_MODE = not ('__compiled__' in globals())
 
 if IS_IN_DEBUG_MODE:
-    LAUNCHER_DATA_DIR_PATH = os.path.join(os.getenv('APPDATA'), 'NickelLauncherTest')
+    ROOT = Root(Path(os.getenv('APPDATA')) / 'NickelLauncherTest')
 else:
-    LAUNCHER_DATA_DIR_PATH = os.path.join(os.getenv('APPDATA'), 'NickelLauncher')
-
-VERSIONS_DIR_PATH = os.path.join(LAUNCHER_DATA_DIR_PATH, 'versions')
-INSTANCES_DIR_PATH = os.path.join(LAUNCHER_DATA_DIR_PATH, 'instances')
-TEMP_DIR_PATH = os.path.join(LAUNCHER_DATA_DIR_PATH, 'temp')
-LOGS_DIR_PATH = os.path.join(LAUNCHER_DATA_DIR_PATH, 'logs')
+    ROOT = Root(Path(os.getenv('APPDATA')) / 'NickelLauncher')
