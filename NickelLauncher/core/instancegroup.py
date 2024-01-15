198fb2389cfd68af1261564b118b94c92f53153b
nbuts <nbutskyi@gmail.com>
2024-01-16 00:59:57 +0200

    continue adding the observer pattern

--- NickelLauncher/core/instancegroup.py
+++ NickelLauncher/core/instancegroup.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Callable, TypedDict, Any
 
 from ordered_set import OrderedSet
@@ -7,10 +8,9 @@ from core.instance import Instance
 
 class InstanceGroup:
     def __init__(self, name: str, instances: list[Instance], hidden: bool = False):
-        self.instances = instances
-        self.hidden = hidden
-
         self._name = name.strip()
+        self._hidden = hidden
+        self._instances = instances
 
         self._subscribers: OrderedSet[Callable[[], Any]] = OrderedSet()
 
@@ -21,6 +21,29 @@ class InstanceGroup:
     @name.setter
     def name(self, name: str):
         self._name = name.strip()
+        self._notify_subscribers()
+
+    @property
+    def hidden(self) -> bool:
+        return self._hidden
+
+    @hidden.setter
+    def hidden(self, hidden: bool):
+        self._hidden = hidden
+        self._notify_subscribers()
+
+    @property
+    def instances(self) -> tuple[Instance, ...]:
+        return tuple(self._instances)
+
+    def add_instance(self, instance: Instance, position: int):
+        pass
+
+    def remove_instance(self, instance: Instance):
+        pass
+
+    def move_instance(self, instance: Instance, instance_group: InstanceGroup, position: int):
+        pass
 
     def to_dict(self) -> TypedDict('', {'name': str, 'hidden': bool, 'instances': list[str]}):
         return {
