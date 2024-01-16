c4f6bee066d62be75e273e1bd070b6601c9fb47a
nbuts <nbutskyi@gmail.com>
2024-01-16 16:51:30 +0200

    move away from a complicated path system

--- NickelLauncher/env.py
+++ NickelLauncher/env.py
@@ -1,30 +1,5 @@
 import os
 from pathlib import Path
 
-from customtypes import UserPath
 
-
-class Root(UserPath):
-    @property
-    def versions(self) -> Path:
-        return self / 'versions'
-
-    @property
-    def instances(self) -> Path:
-        return self / 'instances'
-
-    @property
-    def temp(self) -> Path:
-        return self / 'temp'
-
-    @property
-    def logs(self) -> Path:
-        return self / 'logs'
-
-
-IS_IN_DEBUG_MODE = not ('__compiled__' in globals())
-
-if IS_IN_DEBUG_MODE:
-    ROOT = Root(Path(os.getenv('APPDATA')) / 'NickelLauncherTest')
-else:
-    ROOT = Root(Path(os.getenv('APPDATA')) / 'NickelLauncher')
+ROOT = Path(os.getenv('APPDATA')) / 'NickelLauncherTest' if not ('__compiled__' in globals()) else 'NickelLauncher'
