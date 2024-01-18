0704ba05d9d4cf65c1ab75e9c562414c36afc573
nbuts <nbutskyi@gmail.com>
2024-01-19 00:35:54 +0200

    reorder dir names

--- NickelLauncher/env.py
+++ NickelLauncher/env.py
@@ -5,4 +5,4 @@ from pathlib import Path
 if '__compiled__' in globals():
     ROOT = Path(os.getenv('APPDATA')) / 'NickelLauncher'
 else:
-    ROOT = Path(__file__).parent.parent / 'NickelLauncher' / 'TestAppData'
+    ROOT = Path(__file__).parent.parent / 'TestAppData' / 'NickelLauncher'
