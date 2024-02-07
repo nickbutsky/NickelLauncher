eee0b3263e6c043f779257eb40429c806db65d79
nbuts <nbutskyi@gmail.com>
2024-02-08 01:44:55 +0200

    fix type hints

--- NickelLauncher/net/request.py
+++ NickelLauncher/net/request.py
@@ -6,14 +6,17 @@ from report import Report, ProgressDetails
 
 
 def download_file(url: str, destination: Path, reporthook: Callable[[Report], Any] | None = None):
-    def _local_reporthook(block_num: int, block_size: int, total_size: int):
+    request.urlretrieve(url, destination, _download_reporthook_wrapper(reporthook) if reporthook else None)
+
+
+def _download_reporthook_wrapper(reporthook: Callable[[Report], Any]) -> Callable[[int, int, int], Any]:
+    def download_reporthook(block_num: int, block_size: int, total_size: int):
         if total_size <= 0:
             reporthook(Report(Report.PROGRESS, 'Downloading'))
             return
         read_so_far = round(float(block_num * block_size) / pow(1024, 2), 1)
-        total_size = round(float(total_size) / pow(1024, 2), 1)
-        if read_so_far > total_size:
-            read_so_far = total_size
-        reporthook(Report(Report.PROGRESS, 'Downloading', ProgressDetails(read_so_far, total_size, 'MB')))
-
-    request.urlretrieve(url, destination, _local_reporthook if reporthook else None)
+        rounded_total_size = round(float(total_size) / pow(1024, 2), 1)
+        if read_so_far > rounded_total_size:
+            read_so_far = rounded_total_size
+        reporthook(Report(Report.PROGRESS, 'Downloading', ProgressDetails(read_so_far, rounded_total_size, 'MB')))
+    return download_reporthook
