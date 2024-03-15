796b41448d824ac3d5862411ad9eaa3ea760acb6
nbuts <nbutskyi@gmail.com>
2024-03-15 16:41:16 +0200

    add files

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// biome-ignore lint/nursery/noNodejsModules: Allow Node.js modules in vite.config.ts
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// biome-ignore lint/style/noDefaultExport: Allow default exports in vite.config.ts
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/frontend")
+    }
+  }
+});
