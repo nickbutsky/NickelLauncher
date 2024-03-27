764b71711f12b0fc909ac4a26ea5cca85d9be285
Nick Butsky <nbutskyi@gmail.com>
2024-03-27 14:35:24 +0200

    rewrite with pydantic

--- src/backend/instancemanager/stateload.py
+++ src/backend/instancemanager/stateload.py
@@ -1,9 +1,8 @@
-from typing import Iterable, Any
+from __future__ import annotations
+from typing import Iterable, Self
 from pathlib import Path
-import itertools
-import json
 
-from schema import Schema, Or  # pyright: ignore [reportMissingTypeStubs]
+from pydantic import BaseModel, ValidationError, field_validator, model_validator
 
 from state import State
 from core.instance import Instance
@@ -14,106 +13,145 @@ from core.version import Version
 def load_state(directory: Path, versions: Iterable[Version]) -> State:
     try:
         with (directory / "groups.json").open() as f:
-            contents = json.load(f)
-    except (OSError, json.JSONDecodeError):
+            data = f.read()
+        groups_model = _GroupsModel.model_validate_json(data, strict=True)
+    except (OSError, ValidationError):
         return State(_load_instance_groups([], directory, versions), None, directory)
 
-    if not _are_groups_json_contents_valid(contents):
-        return State(_load_instance_groups([], directory, versions), None, directory)
+    groups_model.adapt_to_directory(directory)
 
-    instance_groups = _load_instance_groups(contents["groups"], directory, versions)
-    last_instance = _get_last_instance(contents["last_instance"], instance_groups)
+    instance_groups = _load_instance_groups(groups_model.groups, directory, versions)
+    last_instance = _get_last_instance(groups_model.last_instance, instance_groups)
     return State(instance_groups, last_instance, directory)
 
 
-def _are_groups_json_contents_valid(contents: dict[Any, Any]) -> bool:
-    if not Schema(
-        {
-            "format_version": int,
-            "groups": [
-                {
-                    "name": str,
-                    "hidden": bool,
-                    "instances": [str]
-                }
-            ],
-            "last_instance": Or(str, None)
-        }
-    ).is_valid(contents):  # pyright: ignore [reportUnknownMemberType]
-        return False
+class _GroupsModel(BaseModel):
+    format_version: int
+    groups: list[_GroupModel]
+    last_instance: str | None
 
-    group_names: list[str] = []
-    instance_dir_names: list[str] = []
-    for group_dict in contents["groups"]:
-        group_names.append(group_dict["name"].strip())
-        instance_dir_names += group_dict["instances"]
+    def adapt_to_directory(self, directory: Path) -> None:
+        existing_dirnames = [item.name for item in directory.iterdir() if item.is_dir()]
+        mentioned_existing_dirnames: list[str] = []
+        for group_model in self.groups:
+            group_model.instances[:] = [
+                instance_dirname for instance_dirname in group_model.instances if instance_dirname in existing_dirnames
+            ]
+            mentioned_existing_dirnames += group_model.instances
 
-    return (len(group_names) == len(set(group_names))) and (len(instance_dir_names) == len(set(instance_dir_names)))
+        self.groups[:] = [group_model for group_model in self.groups if group_model.instances]
+
+        not_mentioned_existing_dirnames = [
+            dirname for dirname in existing_dirnames if dirname not in mentioned_existing_dirnames
+        ]
+
+        if not not_mentioned_existing_dirnames:
+            return
+
+        if not self.groups or self.groups[0].name != "":
+            self.groups.insert(0, _GroupModel(name="", hidden=False, instances=[]))
+        self.groups[0].instances += not_mentioned_existing_dirnames
+
+    @field_validator("last_instance")
+    @classmethod
+    def _validate_last_instance_dirname(cls, last_instance_dirname: str | None) -> str | None:
+        if (last_instance_dirname is not None) and (len(last_instance_dirname.split()) != 1):
+            raise ValidationError("Whitespace or empty strings is not allowed")  # noqa: EM101, TRY003
+        return last_instance_dirname
+
+    @model_validator(mode="after")
+    def _finish_validation(self) -> Self:
+        group_names: list[str] = []
+        instance_dirnames: list[str] = []
+        for group_model in self.groups:
+            group_names.append(group_model.name)
+            instance_dirnames += group_model.instances
+
+        if not (
+            (len(group_names) == len(set(group_names))) and (len(instance_dirnames) == len(set(instance_dirnames)))
+        ):
+            raise ValidationError("Group names and instance dirnames must be unique")  # noqa: EM101, TRY003
+
+        for i, group_model in enumerate(self.groups):
+            if group_model.name == "" and i != 0:
+                raise ValidationError("The unnamed group must be at the top")  # noqa: EM101, TRY003
+
+        return self
+
+
+class _GroupModel(BaseModel):
+    name: str
+    hidden: bool
+    instances: list[str]
+
+    @field_validator("name")
+    @classmethod
+    def _validate_name(cls, name: str) -> str:
+        if name.strip() != name:
+            raise ValidationError("Leading and trailing whitespace is not allowed")  # noqa: EM101, TRY003
+        return name
+
+    @field_validator("instances")
+    @classmethod
+    def _validate_instance_dirnames(cls, instance_dirnames: list[str]) -> list[str]:
+        for instance_dirname in instance_dirnames:
+            if len(instance_dirname.split()) != 1:
+                raise ValidationError("Whitespace is not allowed")  # noqa: EM101, TRY003
+        return instance_dirnames
 
 
 def _load_instance_groups(
-        group_dicts: list[dict[str, Any]], directory: Path, versions: Iterable[Version]
+        group_models: list[_GroupModel], directory: Path, versions: Iterable[Version]
 ) -> list[InstanceGroup]:
-    grouped_instance_dirs = itertools.chain.from_iterable(
-        [[directory / dir_name for dir_name in group_dict["instances"]] for group_dict in group_dicts]
-    )
-
-    ungrouped_instance_dirs = [
-        item for item in directory.iterdir() if item.is_dir() and item not in grouped_instance_dirs
-    ]
-
-    ungrouped_instances = [
-        instance for instance in [
-            _load_instance(directory, versions) for directory in ungrouped_instance_dirs
-        ] if instance is not None
-    ]
-
     groups: list[InstanceGroup] = []
-    for group_dict in group_dicts:
+    for group_model in group_models:
         instances = [
             instance for instance in [
-                _load_instance(directory / dir_name, versions) for dir_name in group_dict["instances"]
+                _load_instance(directory / dirname, versions) for dirname in group_model.instances
             ] if instance is not None
         ]
-        if group_dict["name"] == "":
-            groups.insert(0, InstanceGroup("", instances + ungrouped_instances))
-        else:
-            groups.append(InstanceGroup(group_dict["name"], instances, group_dict["hidden"]))
+        if not instances:
+            break
+        groups.append(InstanceGroup(group_model.name, instances, group_model.hidden))
 
-    return [group for group in groups if group.instances] if groups else [InstanceGroup("", ungrouped_instances)]
+    return groups
 
 
-def _load_instance(instance_directory: Path, versions: Iterable[Version]) -> Instance | None:
-    try:
-        with (instance_directory / "config.json").open() as f:
-            config = json.load(f)
-    except (OSError, json.JSONDecodeError):
+class _InstanceModel(BaseModel):
+    format_version: int
+    name: str
+    version: _VersionModel
+
+
+class _VersionModel(BaseModel):
+    name: str
+    architecture_choice: str
+
+
+def _load_instance(directory: Path, versions: Iterable[Version]) -> Instance | None:
+    if len(directory.name.split()) != 1:
         return None
-    if not Schema(
-        {
-            "format_version": int,
-            "name": str,
-            "version": {
-                "name": str,
-                "architecture_choice": str
-            }
-        }
-    ).is_valid(config):  # pyright: ignore [reportUnknownMemberType]
-        return None
+
     try:
+        with (directory / "config.json").open() as f:
+            data = f.read()
+        instance_model = _InstanceModel.model_validate_json(data, strict=True)
         version = next(
-            v for v in versions if (v.name == config["version"]["name"])
-            and (config["version"]["architecture_choice"] in v.available_architectures)
+            v for v in versions if (v.name == instance_model.version.name)
+            and (instance_model.version.architecture_choice in v.available_architectures)
         )
-    except StopIteration:
+    except (OSError, ValidationError, StopIteration):
         return None
-    instance = Instance(config["name"], version, config["version"]["architecture_choice"], instance_directory)
-    return instance if (instance_directory / "com.mojang").is_dir() else None
+
+    instance = Instance(instance_model.name, version, instance_model.version.architecture_choice, directory)  # pyright: ignore [reportArgumentType]
+    return instance if (directory / "com.mojang").is_dir() else None
 
 
-def _get_last_instance(instance_dir_name: str, instance_groups: list[InstanceGroup]) -> Instance | None:
+def _get_last_instance(instance_dirname: str | None, instance_groups: list[InstanceGroup]) -> Instance | None:
+    if not instance_dirname:
+        return None
     for group in instance_groups:
         for instance in group.instances:
-            if instance.directory.name == instance_dir_name:
+            if instance.directory.name == instance_dirname:
                 return instance
     return None
