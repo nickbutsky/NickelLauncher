82739dfb7e436581440411883c6cd9e466a9f1cc
Nick Butsky <nbutskyi@gmail.com>
2024-04-12 13:38:32 +0300

    new utility type

--- src/frontend/utils.ts
+++ src/frontend/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
