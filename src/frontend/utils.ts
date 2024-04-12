63841f38d4ff0b09986c2f024e1bf150ff437666
Nick Butsky <nbutskyi@gmail.com>
2024-04-13 00:36:58 +0300

    add timeout

--- src/frontend/utils.ts
+++ src/frontend/utils.ts
@@ -9,9 +9,19 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function runWhenTrue(conditionFunction: () => boolean, functionToRun: () => void, interval = 10) {
+export function runWhenTrue(
+  conditionFunction: () => boolean,
+  functionToRun: () => void,
+  interval = 10,
+  timeout = 10000
+) {
+  let timePassed = 0;
   (function wait() {
+    if (timePassed >= timeout) {
+      return;
+    }
     if (!conditionFunction()) {
+      timePassed += interval;
       setTimeout(() => wait(), interval);
       return;
     }
