79852118b64de87dba48decd81274ba039d6bf17
Nick Butsky <nbutskyi@gmail.com>
2024-04-13 11:49:22 +0300

    rewrite as async

--- src/frontend/utils.ts
+++ src/frontend/utils.ts
@@ -9,22 +9,25 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function runWhenTrue(
+export async function waitUntilTrue(
   conditionFunction: () => boolean,
-  functionToRun: () => void,
   interval = 10,
-  timeout = 10000
+  timeout = 10000,
+  throwOnTimeout = false
 ) {
   let timePassed = 0;
-  (function wait() {
+  return new Promise<void>(function poll(resolve, reject) {
     if (timePassed >= timeout) {
-      return;
+      if (throwOnTimeout) {
+        return reject();
+      }
+      return resolve();
     }
     if (!conditionFunction()) {
       timePassed += interval;
-      setTimeout(() => wait(), interval);
+      setTimeout(() => poll(resolve, reject), interval);
       return;
     }
-    functionToRun();
-  })();
+    resolve();
+  });
 }
