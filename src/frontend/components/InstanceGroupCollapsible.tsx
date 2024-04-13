4ea84fa37f342c1b4a0f4c94e94cd91c0a015e4d
Nick Butsky <nbutskyi@gmail.com>
2024-04-13 21:34:00 +0300

    changes

--- src/frontend/components/InstanceGroupCollapsible.tsx
+++ src/frontend/components/InstanceGroupCollapsible.tsx
@@ -2,20 +2,30 @@ import * as React from "react";
 
 import { CaretDownIcon, CaretRightIcon } from "@radix-ui/react-icons";
 
-import type { Prettify } from "@/utils";
+import { type Prettify, waitUntilTrue } from "@/utils";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
 import { Button } from "@/components/ui/button";
 import { InstanceButton } from "@/components/InstanceButton";
 import { EditableLabel } from "@/components/EditableLabel";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuShortcut,
+  ContextMenuTrigger
+} from "@/components/ui/context-menu";
 
 interface Props {
-  name: string;
-  hidden: boolean;
-  instances: Prettify<React.ComponentProps<typeof InstanceButton>>[];
+  readonly name: string;
+  readonly hidden: boolean;
+  readonly instances: readonly Prettify<React.ComponentProps<typeof InstanceButton>>[];
 }
 
 export function InstanceGroupCollapsible({ name, hidden, instances }: Props) {
   const [state, setState] = React.useState(!hidden);
+
+  const editableLabelRef = React.useRef<React.ComponentRef<typeof EditableLabel>>(null);
+  const renameContextMenuItemRef = React.useRef<React.ComponentRef<typeof ContextMenuItem>>(null);
 
   return (
     <Collapsible defaultOpen={!hidden} onOpenChange={(open) => setState(open)}>
@@ -25,7 +35,26 @@ export function InstanceGroupCollapsible({ name, hidden, instances }: Props) {
             {state ? <CaretDownIcon /> : <CaretRightIcon />}
           </Button>
         </CollapsibleTrigger>
-        <EditableLabel defaultValue={name} />
+        <ContextMenu>
+          <ContextMenuTrigger asChild={true}>
+            <div>
+              <EditableLabel ref={editableLabelRef} defaultValue={name} />
+            </div>
+          </ContextMenuTrigger>
+          <ContextMenuContent>
+            <ContextMenuItem
+              ref={renameContextMenuItemRef}
+              onSelect={() =>
+                waitUntilTrue(() => !renameContextMenuItemRef.current).then(() =>
+                  editableLabelRef.current?.enterEditMode()
+                )
+              }
+            >
+              Rename
+              <ContextMenuShortcut>F2</ContextMenuShortcut>
+            </ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>
       </div>
       <CollapsibleContent className="data-[state=closed]:hidden" forceMount={true}>
         {instances.map((instance) => (
