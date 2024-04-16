9df0538173e3f4aabad214d75815f96ef20e783e
Nick Butsky <nbutskyi@gmail.com>
2024-04-16 10:33:18 +0300

    forwardref

--- src/frontend/components/InstanceGroupCollapsible.tsx
+++ src/frontend/components/InstanceGroupCollapsible.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { CaretDownIcon, CaretRightIcon } from "@radix-ui/react-icons";
+import { CaretDownIcon } from "@radix-ui/react-icons";
 
 import { type Prettify, waitUntilTrue } from "@/utils";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
@@ -21,62 +21,65 @@ interface Props {
   readonly instances: readonly Prettify<React.ComponentProps<typeof InstanceButton>>[];
 }
 
-export function InstanceGroupCollapsible({ name, hidden, instances }: Props) {
-  const [state, setState] = React.useState(!hidden);
+export const InstanceGroupCollapsible = React.forwardRef(
+  (
+    { name, hidden, instances, defaultOpen, ...props }: Props & React.ComponentProps<typeof Collapsible>,
+    forwardedRef: React.ForwardedRef<HTMLDivElement>
+  ) => {
+    const editableLabelRef = React.useRef<React.ComponentRef<typeof EditableLabel>>(null);
+    const renameContextMenuItemRef = React.useRef<React.ComponentRef<typeof ContextMenuItem>>(null);
 
-  const editableLabelRef = React.useRef<React.ComponentRef<typeof EditableLabel>>(null);
-  const renameContextMenuItemRef = React.useRef<React.ComponentRef<typeof ContextMenuItem>>(null);
-
-  return (
-    <Collapsible defaultOpen={!hidden} onOpenChange={(open) => setState(open)}>
-      <div className="flex items-center gap-2">
-        <CollapsibleTrigger asChild={true}>
-          <Button variant="ghost" size="icon">
-            {state ? <CaretDownIcon /> : <CaretRightIcon />}
-          </Button>
-        </CollapsibleTrigger>
-        {name ? (
-          <ContextMenu>
-            <ContextMenuTrigger asChild={true}>
-              <div>
-                <EditableLabel
-                  ref={editableLabelRef}
-                  defaultValue={name}
-                  maxLength={50}
-                  applyOnAboutToSave={(value) => value.trim()}
-                  isAllowedToSave={(value) => value.length > 0}
-                />
-              </div>
-            </ContextMenuTrigger>
-            <ContextMenuContent>
-              <ContextMenuItem
-                ref={renameContextMenuItemRef}
-                onSelect={() =>
-                  waitUntilTrue(() => !renameContextMenuItemRef.current).then(() =>
-                    editableLabelRef.current?.enterEditMode()
-                  )
-                }
-              >
-                Rename
-                <ContextMenuShortcut>F2</ContextMenuShortcut>
-              </ContextMenuItem>
-            </ContextMenuContent>
-          </ContextMenu>
-        ) : (
-          <div>{name}</div>
-        )}
-      </div>
-      <CollapsibleContent className="data-[state=closed]:hidden" forceMount={true}>
-        {instances.map((instance) => (
-          <InstanceButton
-            key={instance.name}
-            name={instance.name}
-            displayVersionName={instance.displayVersionName}
-            architectureChoice={instance.architectureChoice}
-            availableArchitectures={instance.availableArchitectures}
-          />
-        ))}
-      </CollapsibleContent>
-    </Collapsible>
-  );
-}
+    return (
+      <Collapsible ref={forwardedRef} defaultOpen={!hidden} {...props}>
+        <div className="flex items-center gap-2">
+          <CollapsibleTrigger asChild={true}>
+            <Button className="data-[state=closed]:-rotate-90" variant="ghost" size="icon">
+              <CaretDownIcon />
+            </Button>
+          </CollapsibleTrigger>
+          {name ? (
+            <ContextMenu>
+              <ContextMenuTrigger asChild={true}>
+                <div>
+                  <EditableLabel
+                    ref={editableLabelRef}
+                    defaultValue={name}
+                    maxLength={50}
+                    applyOnAboutToSave={(value) => value.trim()}
+                    isAllowedToSave={(value) => value.length > 0}
+                  />
+                </div>
+              </ContextMenuTrigger>
+              <ContextMenuContent>
+                <ContextMenuItem
+                  ref={renameContextMenuItemRef}
+                  onSelect={() =>
+                    waitUntilTrue(() => !renameContextMenuItemRef.current).then(() =>
+                      editableLabelRef.current?.enterEditMode()
+                    )
+                  }
+                >
+                  Rename
+                  <ContextMenuShortcut>F2</ContextMenuShortcut>
+                </ContextMenuItem>
+              </ContextMenuContent>
+            </ContextMenu>
+          ) : (
+            <div>{name}</div>
+          )}
+        </div>
+        <CollapsibleContent className="mt-1 data-[state=closed]:hidden" forceMount={true}>
+          {instances.map((instance) => (
+            <InstanceButton
+              key={instance.name}
+              name={instance.name}
+              displayVersionName={instance.displayVersionName}
+              architectureChoice={instance.architectureChoice}
+              availableArchitectures={instance.availableArchitectures}
+            />
+          ))}
+        </CollapsibleContent>
+      </Collapsible>
+    );
+  }
+);
