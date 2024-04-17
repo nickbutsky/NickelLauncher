a46bc03de4c1161ad8c339618c698d8c86c6d70d
Nick Butsky <nbutskyi@gmail.com>
2024-04-17 13:46:54 +0300

    refactor

--- src/frontend/components/VersionSelector.tsx
+++ src/frontend/components/VersionSelector.tsx
@@ -3,23 +3,33 @@ import * as React from "react";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/utils";
 
 const versionTypes = ["release", "beta", "preview"] as const;
 
-interface Props {
-  readonly release: readonly Version[];
-  readonly beta: readonly Version[];
-  readonly preview: readonly Version[];
+interface VersionsProps {
+  readonly release: readonly VersionProps[];
+  readonly beta: readonly VersionProps[];
+  readonly preview: readonly VersionProps[];
 }
 
-interface Version {
+interface VersionProps {
   readonly displayName: string;
   readonly availableArchitectures: readonly string[];
 }
 
-export function VersionSelector(props: Props) {
+export const VersionSelector = React.forwardRef<
+  React.ElementRef<typeof Tabs>,
+  React.ComponentPropsWithoutRef<typeof Tabs> & VersionsProps
+>(({ className, defaultValue, release, beta, preview, ...props }, ref) => {
+  const versionsProps = {
+    release: release,
+    beta: beta,
+    preview: preview
+  };
+
   return (
-    <Tabs className="w-[400px]" defaultValue={versionTypes[0]}>
+    <Tabs className={cn("w-[400px]", className)} ref={ref} defaultValue={defaultValue || versionTypes[0]} {...props}>
       <TabsList className="grid w-full grid-cols-3">
         {versionTypes.map((versionType) => (
           <TabsTrigger key={versionType} value={versionType}>
@@ -29,14 +39,14 @@ export function VersionSelector(props: Props) {
       </TabsList>
       {versionTypes.map((versionType) => (
         <TabsContent className="data-[state=inactive]:hidden" key={versionType} value={versionType} forceMount={true}>
-          <InnerVersionSelector versions={props[versionType]} />
+          <InnerVersionSelector versions={versionsProps[versionType]} />
         </TabsContent>
       ))}
     </Tabs>
   );
-}
+});
 
-function InnerVersionSelector({ versions }: { readonly versions: Props[keyof Props] }) {
+function InnerVersionSelector({ versions }: { readonly versions: VersionsProps[keyof VersionsProps] }) {
   const [displayName, setDisplayName] = React.useState(versions[0]?.displayName);
 
   return (
