b04fca942f0c4a093f94c78c175cfe773100bede
Nick Butsky <nbutskyi@gmail.com>
2024-04-27 01:14:50 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from "vite";
 
 // biome-ignore lint/style/noDefaultExport: Allow default exports in vite.config.ts
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    outDir: "../bundled-frontend",
+  },
   plugins: [react()],
   resolve: {
     alias: {
