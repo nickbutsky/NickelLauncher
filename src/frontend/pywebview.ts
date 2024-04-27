12cc189e331c15a69b5d91cee9927397e1152dc9
Nick Butsky <nbutskyi@gmail.com>
2024-04-27 20:05:20 +0300

    start exposing the api

---
+++ src/frontend/pywebview.ts
@@ -0,0 +1,19 @@
+import type { DeepestReadonly } from "@/utils";
+
+declare global {
+  interface Window extends DeepestReadonly<{ pywebview: { api: API } }> {}
+
+  const pywebview: DeepestReadonly<{ api: API }>;
+}
+
+// biome-ignore lint/style/useNamingConvention: False positive
+interface API {
+  getGroups: () => {
+    name: string;
+    instances: {
+      name: string;
+      dirname: string;
+      version: { displayName: string; availableArchitectures: string[] };
+    }[];
+  };
+}
