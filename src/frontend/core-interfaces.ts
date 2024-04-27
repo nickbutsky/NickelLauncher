380b94bc7743cce2e02d324f4ec16c80dab09708
Nick Butsky <nbutskyi@gmail.com>
2024-04-27 20:29:35 +0300

    new structs

---
+++ src/frontend/core-interfaces.ts
@@ -0,0 +1,17 @@
+import type { DeepestReadonly } from "@/utils";
+
+export interface Instance
+  extends DeepestReadonly<{
+    name: string;
+    dirname: string;
+    version: Version;
+  }> {}
+
+export interface Version extends DeepestReadonly<{ displayName: string; availableArchitectures: string[] }> {}
+
+export interface InstanceGroup
+  extends DeepestReadonly<{
+    name: string;
+    hidden: boolean;
+    instances: Instance[];
+  }> {}
