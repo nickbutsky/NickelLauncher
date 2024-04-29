eb22d45c4d286f219193a60734c05e0f6a1f6f95
Nick Butsky <nbutskyi@gmail.com>
2024-04-29 23:21:26 +0300

    differentiate between prod and dev

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -4,10 +4,16 @@ import * as ReactDOM from "react-dom/client";
 import { App } from "@/App.tsx";
 import "@/globals.css";
 
-window.addEventListener("pywebviewready", () => {
+function createRoot() {
   ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
   );
-});
+}
+
+if (import.meta.env.DEV) {
+  createRoot();
+} else {
+  window.addEventListener("pywebviewready", createRoot);
+}
