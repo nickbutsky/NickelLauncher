471a835d0f7ef9dfc4ee0ba74ebac2acaf78bc8c
Nick Butsky <nbutskyi@gmail.com>
2024-05-27 13:53:39 +0300

    remove dead code

--- src/frontend/utils.ts
+++ src/frontend/utils.ts
@@ -2,15 +2,6 @@ import { type ClassValue, clsx } from "clsx";
 import * as React from "react";
 import { twMerge } from "tailwind-merge";
 
-export type Prettify<T> = {
-  [K in keyof T]: T[K];
-} & {};
-
-// biome-ignore lint/style/useNamingConvention: False positive
-export type ModifyReturnType<T extends (...args: Parameters<T>) => void, RT> = T extends object
-  ? (...args: Parameters<T>) => RT
-  : never;
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
