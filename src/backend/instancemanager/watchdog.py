24bbaee26248554e026c30b7db4f86972f77b871
Nick Butsky <nbutskyi@gmail.com>
2024-05-28 11:13:11 +0300

    implement a context manager

--- src/backend/instancemanager/watchdog.py
+++ src/backend/instancemanager/watchdog.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import contextlib
 import logging
 import time
 from typing import TYPE_CHECKING
@@ -9,7 +10,7 @@ from watchdog.observers import Observer
 
 if TYPE_CHECKING:
     from pathlib import Path
-    from typing import Callable
+    from typing import Callable, Generator
 
 
 class Watchdog:
@@ -18,12 +19,13 @@ class Watchdog:
         self._directory = directory
         self._event_handler = _EventHandler(callback)
 
-    @property
-    def tracking_dir_created_event(self) -> bool:
-        return not self._event_handler.ignore_dir_created_event
-
-    def toggle_dir_created_event_tracking(self) -> None:
-        self._event_handler.ignore_dir_created_event = not self._event_handler.ignore_dir_created_event
+    @contextlib.contextmanager
+    def disable_dir_created_event_tracking(self) -> Generator[None, None, None]:
+        self._event_handler.ignore_dir_created_event = True
+        try:
+            yield
+        finally:
+            self._event_handler.ignore_dir_created_event = False
 
     def run(self) -> None:
         observer = Observer()
