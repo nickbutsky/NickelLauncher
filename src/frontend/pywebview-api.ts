fe7c702ac9c390c789c59e0777d689558a66491a
Nick Butsky <nbutskyi@gmail.com>
2024-05-28 12:43:49 +0300

    more immutability

--- src/frontend/pywebview-api.ts
+++ src/frontend/pywebview-api.ts
@@ -3,22 +3,21 @@ import type { DeepReadonly } from "ts-essentials";
 import type { InstanceGroup, VersionsByType } from "@/core-types";
 
 declare global {
-  interface Window extends DeepReadonly<{ pywebview: { api: API } }> {}
+  interface Window extends DeepReadonly<{ pywebview: typeof pywebview }> {}
 
-  const pywebview: DeepReadonly<{ api: API }>;
-}
-
-// biome-ignore lint/style/useNamingConvention: False positive
-interface API {
-  getInstanceGroups: () => Promise<DeepReadonly<InstanceGroup[]>>;
-  getVersionsByType: (remotely?: boolean) => Promise<DeepReadonly<VersionsByType>>;
-  renameInstanceGroup: (oldName: string, newName: string) => Promise<void>;
-  toggleInstanceGroupHidden: (name: string) => Promise<void>;
-  deleteInstanceGroup: (name: string) => Promise<void>;
-  moveInstances: (position: number, groupName: string, dirnames: DeepReadonly<string[]>) => Promise<void>;
-  renameInstance: (dirname: string, newName: string) => Promise<void>;
-  changeVersion: (dirname: string, versionDisplayName: string) => Promise<void>;
-  changeArchitectureChoice: (dirname: string, architectureChoice: string) => Promise<void>;
-  copyInstance: (dirname: string, copyWorlds: boolean) => Promise<void>;
-  createInstance: (name: string, groupName: string, versionDisplayName: string) => Promise<void>;
+  const pywebview: DeepReadonly<{
+    api: {
+      getInstanceGroups: () => Promise<DeepReadonly<InstanceGroup[]>>;
+      getVersionsByType: (remotely?: boolean) => Promise<DeepReadonly<VersionsByType>>;
+      renameInstanceGroup: (oldName: string, newName: string) => Promise<void>;
+      toggleInstanceGroupHidden: (name: string) => Promise<void>;
+      deleteInstanceGroup: (name: string) => Promise<void>;
+      moveInstances: (position: number, groupName: string, dirnames: DeepReadonly<string[]>) => Promise<void>;
+      renameInstance: (dirname: string, newName: string) => Promise<void>;
+      changeVersion: (dirname: string, versionDisplayName: string) => Promise<void>;
+      changeArchitectureChoice: (dirname: string, architectureChoice: string) => Promise<void>;
+      copyInstance: (dirname: string, copyWorlds: boolean) => Promise<void>;
+      createInstance: (name: string, groupName: string, versionDisplayName: string) => Promise<void>;
+    };
+  }>;
 }
