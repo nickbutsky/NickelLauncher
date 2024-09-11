ac9976ec0d80c2721275f928d3791dd1b72b31eb
Nick Butsky <nbutskyi@gmail.com>
2024-09-11 13:58:35 +0300

    simplify configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // biome-ignore lint/style/noDefaultExport: Allow default exports in vite.config.ts
 export default defineConfig({
@@ -9,11 +10,6 @@ export default defineConfig({
     emptyOutDir: true,
     outDir: "../bundled-frontend",
   },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/frontend"),
-    },
-  },
+  plugins: [tsconfigPaths(), react()],
   root: path.resolve(__dirname, "./src/frontend"),
 });
