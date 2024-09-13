2ded5d7a25922863408cc402dda61a3f3fde842d
Nick Butsky <nbutskyi@gmail.com>
2024-09-13 18:24:49 +0300

    style change

--- src/frontend/components/instance-creation-dialog-content.tsx
+++ src/frontend/components/instance-creation-dialog-content.tsx
@@ -14,6 +14,7 @@ export function InstanceCreationDialogContent() {
   const versionTypeToVersions = useStore((state) => state.versionTypeToVersions);
   const reloadVersionTypeToVersions = useStore((state) => state.reloadVersionTypeToVersions);
   const instanceGroups = useStore((state) => state.instanceGroups);
+  const reloadInstanceGroups = useStore((state) => state.reloadInstanceGroups);
 
   return (
     <FormDialogContent
@@ -31,7 +32,7 @@ export function InstanceCreationDialogContent() {
       }}
       onSubmitBeforeClose={(data) =>
         pywebview.api.createInstance(data.instanceName, data.groupName, data.versionDisplayName).then((dirname) => {
-          appContext.refreshMainArea();
+          reloadInstanceGroups();
           appContext.scrollToInstance(dirname);
         })
       }
